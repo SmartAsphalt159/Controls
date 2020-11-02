046de654c39aa0aad4ea1ce5033d9e1908b888ab
caymanbarber <barbercayman@gmail.com>
2020-11-02 15:25:26 -0800

    added sensors framework with psudocode (cannot run)

--- ControlSystem.py
+++ ControlSystem.py
@@ -1,5 +1,5 @@
 import time
-
+import Sensors
 
 
 """Call when all sensor have been updated"""
@@ -9,12 +9,19 @@ def onAllUpdated():
     _delta_t = last_updated - _now
     last_updated = _now
 
+def find_target_velocity():
+    comm_reference = get_comm_ref() #pseudocode
+    distance = get_lead_car_delta_distance
+    return target_velocity
 
-
+def motor_value():
+    deltav = find_target_velocity() - sensors.get_linear_speed()
+    #try to change velocity by deltav
 
 def checkUpdated():
 
 
 if __name__=="__main__":
     last_updated = time.time()
+    sensors = Sensors()
     precision = 1 #cm
--- Sensors.py
+++ Sensors.py
@@ -3,6 +3,7 @@ import EncoderLibrary
 import math
 
 wheel_radius = 5 #cm
+filter_distance = 1000 #cm  #distance of what values it should factor in
 
 
 class Sensors():
@@ -14,11 +15,12 @@ class Sensors():
         self.acceleration = 0
         self.leading_car_list = []
 
-    def update_encoder(self):
+    def update_encoder(self,delta_t):
         global wheel_radius
+        old_speed = self.linear_speed
         self.rotational_speed = self.encoder.getSpeed() #hz #pseudocode
         self.linear_speed = self.rotational_speed*2*math.pi*wheel_radius #m/s
-        self
+        self.acceleration = (self.linear_speed-old_speed)/delta_t
 
     def get_rotational_speed(self):
         return self.rotational_speed
@@ -29,16 +31,54 @@ class Sensors():
     def update_lidar(self):
         self.lidar.update() #pseudocode
 
+    """should be quite invloved function with machine vision and tracking objects over time"""
     def interpret_lidar(self):
-        lidar_point_cloud = self.lidar.get_pointcloud()#pseudocode
+        lidar_point_cloud = self.lidar.get_pointcloud()#pseudocode #get in polar
+        cloud_car_list = find_cars(lidar_point_cloud,
+                                    self.lidar.angular_resolution) #pseudocode
+        if can_see_car()
+            and not self.leading_car_list:
+        self.leading_car_list.append(LeadingCar())
 
+
+"""given point cloud from lidar return lists of where it thinks a car is"""
+def find_cars(point_cloud, ang_res):
+    cloud_car_list = []
+    threshold = 15 #cm #what threshold should be used to find edges
+    sobel = [   #can change to other operators
+        -1, 0, 1
+    ]
+
+    for i in point_cloud:
+        if i > filter_distance:
+            point_cloud[i] = filter_distance
+    """Edge detection loop"""
+    for i, point in enumerate(point_cloud):
+        sum = 0
+        for j in sobel:
+            if i == 0 and j == -1:
+                sum += j*point_cloud[len(point_cloud) - 1]
+            elif i == len(point_cloud) - 1 and i == 1:
+                sum += j*point_cloud[0]
+            else:
+                sum += j*point_cloud[i+j]
+
+        if sum > threshold:
+            edges.append(sum)
+        else:
+            edges.append(0)
+
+    """loop through and find possible cars using machine vision"""
+    return cloud_car_list
 
 
 
 class LeadingCar():
     """rel_pos, rel_vel are 2D arrays holding x and y values. Lidar is considered origin"""
     def __init__(self, rel_pos, rel_vel,id):
-        self.rel_pos = rel_pos
+        self.rel_pos = rel_pos #from lidar
         self.rel_vel = rel_vel
         self.id = id
+        #maybe chunk of point cloud that
+
         #add additional machine vision a parameters
